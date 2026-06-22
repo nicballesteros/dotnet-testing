3d2270cddd46d37f88a4ab9e05f9a956d50aee4b
Nic <nicballesteros2@gmail.com>
2026-06-22 18:20:09 +0000

    Init dotnet project and cause a failing test

---
+++ tests/Calculator.Tests/Calculator.Tests.cs
@@ -0,0 +1,61 @@
+namespace Calculator.Tests;
+using Bogus;
+
+
+public class UnitTest1
+{
+    [Fact]
+    public void Calculator_add_sumtwonumbers()
+    {
+        //Assemble
+        var calculator = new Calculator();
+        var faker = new Faker();
+
+        int a = faker.Random.Int();
+        int b = faker.Random.Int();
+
+        //Act
+        int sum = calculator.Add(a, b);
+
+        //Assert
+        Assert.Equal(a + b, sum);
+    }
+
+    [Fact]
+    public void Calculator_divide_twoIntsNotZero()
+    {
+        //Assemble
+        var calculator = new Calculator();
+        var faker = new Faker();
+
+        int a = faker.Random.Int();
+        int b = 0;
+        while (b == 0)
+        {
+            b = faker.Random.Int();
+        }
+
+        //Act
+        int quotient = calculator.Divide(a, b);
+
+        //Assert
+        Assert.Equal(a / b, quotient);
+    }
+
+    [Fact]
+    public void Calculator_divide_anIntByZero()
+    {
+        //Assemble
+        var calculator = new Calculator();
+        var faker = new Faker();
+
+        int a = faker.Random.Int();
+        int b = 0;
+
+        //Act
+        int quotient = calculator.Divide(a, b);
+
+        //Assert
+        Assert.Equal(a / b, quotient);
+    }
+}
